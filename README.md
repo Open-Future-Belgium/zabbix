244f936a32a16b885c5ead9baa122fc41be8a0a4
Patrik Uytterhoeven <patrik@open-future.be>
2015-08-24 10:35:09 +0200

    fixed rhel 6 issues

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 Zabbix Scripts
 ==============
 
-check-yum-updates: works for rhel6 / rhel7 and probably fedora
+check-yum-updates: works for rhel6 / rhel7
+copy the script in cron.daily
+install zabbix-sender
+add template to you zabbix server and link it with the hosts.
 
--- check-yum-updates/check-yum-updates.sh
+++ check-yum-updates/check-yum-updates.sh
@@ -23,11 +23,25 @@ else
   SELINUX=0
 fi
 
+
 ### Check for Security Updates ###
-MODERATE=$(yum updateinfo list --sec-severity=Moderate/ | grep Moderate | wc -l)
-IMPORTANT=$(yum updateinfo list --sec-severity=Important/ | grep Important | wc -l)
-LOW=$(yum updateinfo list --sec-severity=Low | grep Low/ | wc -l)
-CRITICAL=$(yum updateinfo list --sec-severity=Critical/ | grep Critical | wc -l)
+if   grep -q -i "release 6" /etc/redhat-release ; then
+  MODERATE2=$(yum updateinfo list --sec-severity=Moderate | grep Moderate | wc -l)
+  IMPORTANT2=$(yum updateinfo list --sec-severity=Important | grep Important | wc -l)
+  LOW2=$(yum updateinfo list --sec-severity=Low | grep Low | wc -l)
+  CRITICAL2=$(yum updateinfo list --sec-severity=Critical | grep Critical | wc -l)
+  MODERATE=$((MODERATE2 - 1))
+  IMPORTANT=$((IMPORTANT2 - 1))
+  LOW=$((LOW2 - 1))
+  CRITICAL=$((CRITICAL2 - 1))
+elif grep -q -i "release 7" /etc/redhat-release ; then
+  MODERATE=$(yum updateinfo list --sec-severity=Moderate | grep Moderate | wc -l)
+  IMPORTANT=$(yum updateinfo list --sec-severity=Important | grep Important | wc -l)
+  LOW=$(yum updateinfo list --sec-severity=Low | grep Low | wc -l)
+  CRITICAL=$(yum updateinfo list --sec-severity=Critical | grep Critical | wc -l)
+else
+  echo "Running neither RHEL6.x nor RHEL 7.x !"
+fi
 
 
 ### Add data to file and send it to Zabbix Server ###
@@ -38,6 +52,8 @@ echo "$HOSTNAME yum.low $LOW" >> $ZBX_DATA
 echo "$HOSTNAME yum.critical $CRITICAL" >> $ZBX_DATA
 echo "$HOSTNAME yum.release $RELEASE" >> $ZBX_DATA
 echo "$HOSTNAME yum.selinux $SELINUX" >> $ZBX_DATA
+
+
 
 zabbix_sender -z $ZBX_SERVER_IP -i $ZBX_DATA &> /dev/null
 
